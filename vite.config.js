ddd029a423de4fd9f0ad7730acb6aa619bf8086a
sandyyy741821 <santhoshsandyyy741821@gmail.com>
2025-04-08 18:49:46 +0530

    added netlify config

--- vite.config.js
+++ vite.config.js
@@ -7,10 +7,10 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      '/api': 'https://sub-mainfrontendnet.netlify.app/',
+      '/api': 'https://sub-mainfrontendnet.netlify.app',
     },
     port: 443,
-    host: 'sub-mainfrontendnet.netlify.app', 
+    host: '0.0.0.0', 
     // https: {
     //   key: fs.readFileSync(path.resolve(__dirname, 'D:/Internship/server-github-services/sub-frontend/example.com+2-key.pem')),  
     //   cert: fs.readFileSync(path.resolve(__dirname, 'D:/Internship/server-github-services/sub-frontend/example.com+2-cert.pem')),
