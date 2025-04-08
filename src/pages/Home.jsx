6b80459c03d0ed52a30a07f0fa845e1ae4b514bc
sandyyy741821 <santhoshsandyyy741821@gmail.com>
2025-04-08 18:37:37 +0530

    added netlify config

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,21 +3,27 @@ import React, { useState } from 'react';
 const App = () => {
   const [name, setName] = useState('');
   const [apiResponse, setApiResponse] = useState('');
+  const [loading, setLoading] = useState(false); 
 
   const handleSubmit = () => {
+    setLoading(true); 
+    setApiResponse(''); 
+
     fetch('http://13.51.101.168:5000/api', {
-      method: 'POST', 
+      method: 'POST',
       headers: {
         'Content-Type': 'application/json',
       },
-      body: JSON.stringify({ name: name }), 
+      body: JSON.stringify({ name: name }),
     })
       .then(response => response.json())
       .then(data => {
-        setApiResponse(data.message); 
+        setApiResponse(data.message);
+        setLoading(false);
       })
       .catch(error => {
         console.error('Error fetching data from the API:', error);
+        setLoading(false); 
       });
   };
 
@@ -30,9 +36,9 @@ const App = () => {
         onChange={(e) => setName(e.target.value)}
         placeholder="Enter your name"
       />
-      <button onClick={handleSubmit}>Send</button>
+      <button onClick={handleSubmit} disabled={loading}>Send</button>
 
-      <h2>{apiResponse}</h2> 
+      {loading ? <h2>Loading...</h2> : <h2>{apiResponse}</h2>} 
     </div>
   );
 };
